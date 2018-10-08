7548a92adeef6585169feec5aa6d3656fa711bc8
Nathan Nguyen <nathannguyen@catalyst-au.net>
2018-10-08 11:00:24 +1100

    Fix privacy test

--- tests/privacy/provider_test.php
+++ tests/privacy/provider_test.php
@@ -92,19 +92,6 @@ class plagiarism_turnitin_privacy_provider_testcase extends \core_privacy\tests\
 
         $csresponse = $this->create_submission();
 
-        // Set the cm to the correct one for our submission.
-        $cms = $DB->get_records('course_modules');
-        $cm = reset($cms);
-        $submissions = $DB->get_records('plagiarism_turnitin_files');
-        $submission = reset($submissions);
-
-        $update = new stdClass();
-        $update->id = $submission->id;
-        $update->cm = $cm->instance;
-        $DB->update_record('plagiarism_turnitin_files', $update);
-
-        $this->assertEquals(1, count($submissions));
-
         $contextlist = provider::get_contexts_for_userid($csresponse["Student"]->id);
 
         $this->assertCount(1, $contextlist);
